62b3afee5ea9cd4058193c3ea46139c802baf599
Arbin Mahato <arma_csit2078@lict.edu.np>
2025-11-26 13:23:30 +0545

    suspense added

--- myapp/app/cart/page.tsx
+++ myapp/app/cart/page.tsx
@@ -1,9 +1,13 @@
 'use client';
-
+import { Suspense } from "react";
 import CartPage from "@/components/Carts"
 
 function page() {
-  return <CartPage/>
+  return (
+    <Suspense fallback={<p>Loading cart...</p>}>
+      <CartPage />
+    </Suspense>
+  );
 }
 
 export default page
